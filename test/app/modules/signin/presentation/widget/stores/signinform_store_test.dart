bc7558c057d6118d791039f887c2c33101ae2453
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2021-09-24 19:59:23 -0300

    test: ensure EmailController return false if not valid email is provided

--- test/app/modules/signin/presentation/widget/stores/signinform_store_test.dart
+++ test/app/modules/signin/presentation/widget/stores/signinform_store_test.dart
@@ -1,19 +1,20 @@
 import 'package:ezwallet_mobile/app/modules/signin/presentation/protocols/email_validator_protocol.dart';
 import 'package:ezwallet_mobile/app/modules/signin/presentation/widgets/stores/signinform_store.dart';
+import 'package:ezwallet_mobile/app/modules/signin/signin_module.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_test/flutter_test.dart';
+// import 'package:mobx/mobx.dart';
+import 'package:mockito/mockito.dart';
+import 'package:modular_test/modular_test.dart';
 
-class EmailValidatorStub implements EmailValidator {
-  @override
-  validate(String email) => true;
-}
+class EmailValidatorStub extends Mock implements EmailValidator {}
 
 void main() {
-  late SigninFormStore store;
-  late EmailValidatorStub emailValidator;
-  setUpAll(() {
-    emailValidator = EmailValidatorStub();
-    store = SigninFormStore(emailValidator);
-  });
+  var emailValidator = EmailValidatorStub();
+  var store = SigninFormStore(emailValidator);
+
+  setUp(() => initModule(SigninModule(),
+      replaceBinds: [Bind.instance<EmailValidator>(emailValidator)]));
 
   test('should calls email controller with correct value', () async {
     expect(store.email, equals(''));
@@ -29,7 +30,17 @@ void main() {
 
   test('should EmailController return true if valid email is provided',
       () async {
-    var validEmail = store.validateEmail('any_email@mail.com');
+    var email = 'any_email@mail.com';
+    when(emailValidator.validate(email)).thenReturn(true);
+    var validEmail = store.validateEmail(email);
     expect(validEmail, equals(true));
+  });
+
+  test('should EmailController return false if not valid email is provided',
+      () async {
+    var email = '';
+    when(emailValidator.validate(email)).thenReturn(false);
+    var validEmail = store.validateEmail(email);
+    expect(validEmail, equals(false));
   });
 }
