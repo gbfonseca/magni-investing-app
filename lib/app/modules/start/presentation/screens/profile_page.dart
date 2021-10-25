93b0ca428c1c45f2fac3f9c8162ad79373198737
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2021-10-25 18:39:59 -0300

    feat: add Circle avatar and user name

--- lib/app/modules/start/presentation/screens/profile_page.dart
+++ lib/app/modules/start/presentation/screens/profile_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../../../../utils/colors.dart';
 import 'widgets/back_header_widget.dart';
 
 class ProfilePage extends StatelessWidget {
@@ -11,7 +12,31 @@ class ProfilePage extends StatelessWidget {
           child: Container(
             padding: EdgeInsets.all(24),
             child: Column(
-              children: [BackHeader(title: 'Meu Perfil')],
+              children: [
+                BackHeader(title: 'Meu Perfil'),
+                Container(
+                  margin: EdgeInsets.only(top: 64),
+                  child: CircleAvatar(
+                    backgroundColor: ColorConstants.kPrimaryColor,
+                    radius: 56,
+                    backgroundImage: NetworkImage(
+                      'https://images.unsplash.com/photo-1488161628813-04466f872be2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=764&q=80',
+                    ),
+                  ),
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(
+                          color: ColorConstants.kPrimaryColor, width: 4)),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 36, bottom: 72),
+                  child: Text('Gabriel Fonseca',
+                      style: TextStyle(
+                          color: ColorConstants.kFontColor,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 24)),
+                )
+              ],
             ),
           ),
         ),
