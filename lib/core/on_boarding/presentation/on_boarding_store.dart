b0556d0b43a108ce7adc62132a2c44e78bff7035
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2021-12-02 11:18:21 -0300

    fix: on_boarding images path

--- lib/core/on_boarding/presentation/on_boarding_store.dart
+++ lib/core/on_boarding/presentation/on_boarding_store.dart
@@ -10,16 +10,16 @@ abstract class _OnBoardingStoreBase with Store {
   @observable
   List<CarouselItems> value = [
     CarouselItems(
-        path: 'lib/app/assets/images/wallet-image/wallet-image.png',
+        path: 'lib/assets/images/wallet-image/wallet-image.png',
         title: 'Praticidade',
         description: 'Monte sua careira de investimentos facilmente.'),
     CarouselItems(
-        path: 'lib/app/assets/images/personal_finance/personal_finance.png',
+        path: 'lib/assets/images/personal_finance/personal_finance.png',
         title: 'Simplicidade',
         description:
             'Acompanhe a evolução do seu patrimônio de forma simples.'),
     CarouselItems(
-        path: 'lib/app/assets/images/analysis/analysis.png',
+        path: 'lib/assets/images/analysis/analysis.png',
         title: 'Fácil análise',
         description: 'Compare e analise facilmente os ativos.'),
   ];
