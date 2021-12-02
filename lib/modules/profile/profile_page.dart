dcdb9fa9d5ffe5e1dd7dcb12d8a3046e82403b29
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2021-12-02 16:29:40 -0300

    feat: add hero animation of header to profile page

--- lib/modules/profile/profile_page.dart
+++ lib/modules/profile/profile_page.dart
@@ -20,11 +20,14 @@ class ProfilePage extends StatelessWidget {
                 ),
                 Container(
                   margin: EdgeInsets.only(top: 64),
-                  child: CircleAvatar(
-                    backgroundColor: ColorConstants.kPrimaryColor,
-                    radius: 56,
-                    backgroundImage: NetworkImage(
-                      'https://images.unsplash.com/photo-1488161628813-04466f872be2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=764&q=80',
+                  child: Hero(
+                    tag: 'avatar',
+                    child: CircleAvatar(
+                      backgroundColor: ColorConstants.kPrimaryColor,
+                      radius: 56,
+                      backgroundImage: NetworkImage(
+                        'https://images.unsplash.com/photo-1488161628813-04466f872be2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=764&q=80',
+                      ),
                     ),
                   ),
                   decoration: BoxDecoration(
