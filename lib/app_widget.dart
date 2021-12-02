6cbad0240d610df070617d5cc9ed86ef179446df
Gabriel Silva <gabriel.silva@calindra.com.br>
2021-12-01 22:55:58 -0300

    fix: routes path

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -6,6 +6,7 @@ import 'core/on_boarding/presentation/on_boarding_page.dart';
 import 'core/signin/presentation/signin_page.dart';
 import 'core/signup/presentation/signup_page.dart';
 import 'modules/add_investment/add_investment_page.dart';
+import 'modules/profile/profile_page.dart';
 import 'modules/settings/settings_page.dart';
 import 'modules/start/presentation/start_page.dart';
 import 'utils/color.dart';
@@ -31,7 +32,8 @@ class AppWidget extends StatelessWidget {
           '/signup/': (context) => SignupPage(),
           '/start/': (context) => StartPage(),
           '/start/add_investment/': (context) => AddInvestmentPage(),
-          '/start/profile/': (context) => SettingsPage()
+          '/start/profile/': (context) => ProfilePage(),
+          '/start/settings/': (context) => SettingsPage(),
         },
       ));
 }
