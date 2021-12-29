075c727a74aa19af1800685d614dd4541d784c88
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2021-12-29 13:06:29 -0300

    refactor:  remove mobx on onboarding and change to ChangeNotifier

--- lib/core/on_boarding/presentation/on_boarding.dart
+++ lib/core/on_boarding/presentation/on_boarding.dart
@@ -1,13 +1,8 @@
-import 'package:mobx/mobx.dart';
+import 'package:flutter/material.dart';
 
 import '../../on_boarding/domain/models/carousel_items.dart';
 
-part 'on_boarding_store.g.dart';
-
-class OnBoardingStore = _OnBoardingStoreBase with _$OnBoardingStore;
-
-abstract class _OnBoardingStoreBase with Store {
-  @observable
+class OnBoardingNotifier extends ChangeNotifier {
   List<CarouselItems> value = [
     CarouselItems(
         path: 'lib/assets/images/wallet-image/wallet-image.png',
