daae185c5782da746979cd0c9a4c3b0211a6d305
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2022-06-19 21:03:05 -0300

    test: ensure Login Page has a login text

--- test/ui/pages/login_page_test.dart
+++ test/ui/pages/login_page_test.dart
@@ -1,1 +1,12 @@
-void main() {}
+import 'package:flutter_test/flutter_test.dart';
+import 'package:magni_investing/ui/pages/login_page.dart';
+
+import '../../factories/create_widget_for_testing.dart';
+
+void main() {
+  testWidgets('should welcome page has a Login Title', (tester) async {
+    await tester.pumpWidget(createWidgetForTesting(child: LoginPage()));
+    final loginTextFinder = find.text('Log in');
+    expect(loginTextFinder, findsOneWidget);
+  });
+}
